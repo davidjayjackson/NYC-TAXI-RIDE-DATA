70cf056917e4c53af949ae0f12237169245ffc9b
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2023-03-31 18:42:09 -0400

    The proof is int the pudding. NYC Taxi rides began declining in 2013.

---
+++ ride_counts_by_date.R
@@ -0,0 +1,31 @@
+library(duckdb)
+library(DBI)
+library(tidyverse)
+library(scales)
+
+con <- dbConnect(duckdb::duckdb(), dbdir = "./nyctaxis.duckdb", read_only = FALSE)
+dbGetQuery(con,"SELECT pickup_datetime,count(*) as p_count  FROM db_cleaner 
+           GROUP BY pickup_datetime
+           ORDER  BY p_count desc;") %>% 
+ggplot(aes(x=pickup_datetime,y=p_count)) + geom_line()
+
+dbGetQuery(con,"
+           SELECT pickup_datetime, COUNT(*) AS p_count  
+            FROM db_cleaner 
+            WHERE pickup_datetime LIKE '%-01-%'
+            GROUP BY pickup_datetime
+            ORDER BY p_count DESC;")%>% 
+            ggplot(aes(x=pickup_datetime,y=p_count)) + geom_col()
+
+dbGetQuery(con,"
+           SELECT pickup_datetime,
+           EXTRACT(year FROM pickup_datetime) AS pickup_year, 
+           COUNT(*) AS ride_count  
+          FROM db_cleaner 
+           WHERE pickup_datetime LIKE '%-01-%'
+          GROUP BY pickup_datetime
+          ORDER BY pickup_datetime;") %>% 
+  ggplot(aes(x=pickup_datetime,y=ride_count)) + geom_smooth(span=0.25) +
+  geom_point(aes(x=pickup_datetime,y=ride_count)) +
+  scale_y_continuous(labels = comma) 
+  
