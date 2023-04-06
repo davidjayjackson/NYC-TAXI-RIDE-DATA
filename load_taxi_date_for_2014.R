462f37aff2f79a86c1dc1fc16e7388e71f3084a0
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2023-04-06 13:27:43 -0400

    Added Yellow Cab data for 2016

--- load_taxi_date_for_2014.R
+++ load_taxi_date_for_2014.R
@@ -14,75 +14,82 @@ con <- dbConnect(duckdb::duckdb(), dbdir = "./nyctaxis.duckdb", read_only = FALS
 #   rename( pickup_datetime = tpep_pickup_datetime) %>%
 #   rename( dropoff_datetime = tpep_dropoff_datetime) 
 # sample0$start_date <- as.Date(sample0$pickup_datetime)
+##
 
-sample1 <- read_parquet("./data/yellow_tripdata_2013-01.parquet") %>%
+sample1 <- read_parquet("./data/yellow_tripdata_2016-01.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime) 
 sample1$start_date <- as.Date(sample1$pickup_datetime)
 
-sample2 <- read_parquet("./data/yellow_tripdata_2013-02.parquet") %>%
+sample2 <- read_parquet("./data/yellow_tripdata_2016-02.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime) 
 sample2$start_date <- as.Date(sample2$pickup_datetime)
 
-sample3 <- read_parquet("./data/yellow_tripdata_2013-03.parquet") %>%
+sample3 <- read_parquet("./data/yellow_tripdata_2016-03.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample3$start_date <- as.Date(sample3$pickup_datetime)
 
-sample4 <- read_parquet("./data/yellow_tripdata_2013-04.parquet") %>%
+sample4 <- read_parquet("./data/yellow_tripdata_2016-04.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample4$start_date <- as.Date(sample4$pickup_datetime)
 
-sample5 <- read_parquet("./data/yellow_tripdata_2013-05.parquet") %>%
+sample5 <- read_parquet("./data/yellow_tripdata_2016-05.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample5$start_date <- as.Date(sample5$pickup_datetime)
 
-sample6 <- read_parquet("./data/yellow_tripdata_2013-06.parquet") %>%
+sample6 <- read_parquet("./data/yellow_tripdata_2016-06.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample6$start_date <- as.Date(sample6$pickup_datetime)
 #
 ## July - Dec 2013
 #
-sample7 <- read_parquet("./data/yellow_tripdata_2013-07.parquet") %>%
+sample7 <- read_parquet("./data/yellow_tripdata_2016-07.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample7$start_date <- as.Date(sample7$pickup_datetime)
 
-sample8 <- read_parquet("./data/yellow_tripdata_2013-08.parquet") %>%
+sample8 <- read_parquet("./data/yellow_tripdata_2016-08.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample8$start_date <- as.Date(sample8$pickup_datetime)
 
-sample9 <- read_parquet("./data/yellow_tripdata_2013-09.parquet") %>%
+sample9 <- read_parquet("./data/yellow_tripdata_2016-09.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample9$start_date <- as.Date(sample9$pickup_datetime)
 
-sample10 <- read_parquet("./data/yellow_tripdata_2013-10.parquet") %>%
+sample10 <- read_parquet("./data/yellow_tripdata_2016-10.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample10$start_date <- as.Date(sample10$pickup_datetime)
 
-sample11 <- read_parquet("./data/yellow_tripdata_2013-11.parquet") %>%
+sample11 <- read_parquet("./data/yellow_tripdata_2016-11.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample11$start_date <- as.Date(sample11$pickup_datetime)
 
-sample12 <- read_parquet("./data/yellow_tripdata_2013-12.parquet") %>%
+sample12 <- read_parquet("./data/yellow_tripdata_2016-12.parquet") %>%
   rename( pickup_datetime = tpep_pickup_datetime) %>%
   rename( dropoff_datetime = tpep_dropoff_datetime)
 sample12$start_date <- as.Date(sample12$pickup_datetime)
 
 ##
 ## Create New table for Jan. 2013 data
-dbWriteTable(con, "yellowcab", sample0,overwrite=TRUE)
-dbGetQuery(con,"
-           SELECT start_date,count(*) 
-          FROM yellowcab ;")
+#dbWriteTable(con, "yellowcab", sample0,overwrite=TRUE)
+## Import Taxi zone data
+taxi_zone <- read_csv("./taxi_zones/taxi_zone_lookup.csv")
+dbWriteTable(con, "zones",taxi_zone,overwrite=TRUE)
+##
+dbGetQuery(con," SELECT count(*),
+           min(start_date),
+           max(start_date)  
+           FROM yellowcab ;")
+
 
 ## Append Feb - Dec onto yellowcab
 dbWriteTable(con, "yellowcab", sample1,overwrite=FALSE,append = TRUE)
